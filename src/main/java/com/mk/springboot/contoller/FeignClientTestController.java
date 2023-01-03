2e22a77a3be35d125cdcf85ddf1d926ce50fa26c
mukesh958 <mukesh958@gmail.com>
2023-01-03 21:32:09 +0530

    RestTemplate

--- src/main/java/com/mk/springboot/contoller/FeignClientTestController.java
+++ src/main/java/com/mk/springboot/contoller/FeignClientTestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.mk.springboot.feign.api.client.UserClient;
 import com.mk.springboot.feign.api.model.UserResponse;
@@ -16,9 +17,19 @@ public class FeignClientTestController {
 
 	@Autowired
 	private UserClient userClient;
-	
+
+	@Autowired
+	RestTemplate restTemplate;
+
 	@GetMapping("/users")
-	public List<UserResponse> getAllDummyUser(){
+	public List<UserResponse> getAllDummyUser() {
 		return userClient.getAllUser();
+	}
+
+	@SuppressWarnings("unchecked")
+	@GetMapping("/usersRestClient")
+	public List<UserResponse> getAllDummyUserRestClient() {
+		return (List<UserResponse>) restTemplate.getForObject("https://jsonplaceholder.typicode.com/users",
+				Object.class);
 	}
 }
